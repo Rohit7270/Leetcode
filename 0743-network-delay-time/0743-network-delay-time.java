9d070592765b2e675b8376dc2815d7089b2460ae
Thiruveedula Rohit <153834179+Rohit7270@users.noreply.github.com>
2026-07-12 12:52:49 +0530

    Time: 8 ms (98.26%), Space: 49.1 MB (53.43%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,9 +1,11 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
         for(int i = 0; i<= n; i++){
             adj.add(new ArrayList<>());
         }
+
         for(int[] edge : times){
             int u = edge[0];
             int v = edge[1];
@@ -11,36 +13,33 @@ class Solution {
 
             adj.get(u).add(new Pair(v, w));
         }
-
+        
         int[] dis = new int[n+1];
         Arrays.fill(dis, (int)1e9);
-        dis[k] = 0;
-        
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.second - y.second);
         pq.offer(new Pair(k, 0));
-
+        dis[k] = 0;
         while(!pq.isEmpty()){
             Pair p = pq.poll();
             int node = p.first;
             int distance = p.second;
             if(distance > dis[node]) continue;
             for(Pair it : adj.get(node)){
-                int next = it.first;
                 int edgewt = it.second;
+                int next = it.first;
                 if(distance + edgewt < dis[next]){
                     dis[next] = distance + edgewt;
                     pq.offer(new Pair(next, dis[next]));
                 }
             }
-        }  
-
+        }
         int ans = 0;
         for(int i = 1; i<= n; i++){
-            if(dis[i] == (int) 1e9){
+            if(dis[i] == (int)1e9){
                 return -1;                
             }
             ans = Math.max(ans, dis[i]);
-        }      
+        }
         return ans;
     }
     class Pair{
